30168412384c56809d8004196052469e40a64d5f
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-02-21 21:38:24 +0100

    download assets is working

--- bin/index.js
+++ bin/index.js
@@ -11,13 +11,57 @@ require('dotenv').config({ silent: true });
 const pogobuf = require("pogobuf");
 const POGOProtos = require("node-pogo-protos");
 const logger = require("winston");
+const Bluebird = require("bluebird");
+const _ = require("lodash");
 const moment = require("moment");
 const fs = require("fs-promise");
+const request = require("request-promise");
+const bfj = require('bfj');
+function getAssetDigest(client) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (fs.existsSync('data/asset.digest.json')) {
+            logger.info('Get asset digest from disk...');
+            let content = yield fs.readFile('data/asset.digest.json', 'utf8');
+            return JSON.parse(content);
+        }
+        else {
+            logger.info('Get asset digest from server...');
+            let assets = yield client.getAssetDigest(1 /* IOS */, '', '', '', 5702);
+            _.each(assets.digest, digest => {
+                // convert buffer to hex string so it's readable
+                digest.key = digest.key.toString('hex');
+            });
+            logger.info('Save asset digest to file...');
+            yield fs.writeFile('data/asset.digest.json', JSON.stringify(assets, null, 4), 'utf8');
+            return assets;
+        }
+    });
+}
+function downloadAssets(client, assets) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (fs.existsSync('data/.skip'))
+            return;
+        let idx = 0;
+        logger.info('Starting to download assets...');
+        yield Bluebird.map(assets.digest, (asset) => __awaiter(this, void 0, void 0, function* () {
+            logger.info('Get asset from %s (%d, %d)', asset.bundle_name, ++idx, assets.digest.length);
+            let response = yield client.getDownloadURLs([asset.asset_id]);
+            let data = yield request.get(response.download_urls[0].url);
+            yield fs.writeFile(`data/${asset.bundle_name}`, data);
+            logger.info('%s done. (%d, %d)', asset.bundle_name, ++idx, assets.digest.length);
+            yield Bluebird.delay(_.random(450, 550));
+        }));
+        yield fs.writeFile('data/.skip', 'skip', 'utf8');
+    });
+}
 function Main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            yield fs.mkdir('data');
+            logger.info('Login...');
             let login = new pogobuf.PTCLogin();
-            login.setProxy(process.env.proxy);
+            if (process.env.proxy)
+                login.setProxy(process.env.proxy);
             let token = yield login.login(process.env.user, process.env.password);
             let client = new pogobuf.Client({
                 authType: 'ptc',
@@ -30,10 +74,12 @@ function Main() {
                 proxy: process.env.proxy,
             });
             yield client.init(false);
-            yield client.batchStart().batchCall();
-            let assets = yield client.getAssetDigest(1 /* IOS */, '', '', '', 5702);
-            let assetsDigests = assets.digest;
-            yield fs.writeFile('assets.digest.json', JSON.stringify(assetsDigests, null, 4), 'utf8');
+            logger.info('First request...');
+            client.batchStart().getPlayer('FR', 'en', 'Europe/Paris');
+            yield client.batchCall();
+            let assets = yield getAssetDigest(client);
+            logger.info('%d assets to download', assets.digest.length);
+            yield downloadAssets(client, assets);
         }
         catch (e) {
             logger.error(e);
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,54 @@ import * as Bluebird from 'bluebird';
 import * as _ from 'lodash';
 import * as moment from 'moment';
 import * as fs from 'fs-promise';
+import * as request from 'request-promise';
+
+const bfj = require('bfj');
+
+async function getAssetDigest(client: pogobuf.Client): Promise<POGOProtos.Networking.Responses.GetAssetDigestResponse> {
+    if (fs.existsSync('data/asset.digest.json')) {
+        logger.info('Get asset digest from disk...');
+        let content = await fs.readFile('data/asset.digest.json', 'utf8');
+        return JSON.parse(content);
+    } else {
+        logger.info('Get asset digest from server...');
+        let assets = await client.getAssetDigest(POGOProtos.Enums.Platform.IOS, '', '', '', 5702);
+        _.each(assets.digest, digest => {
+            // convert buffer to hex string so it's readable
+            (<any>digest).key = digest.key.toString('hex');
+        });
+
+        logger.info('Save asset digest to file...');
+        await fs.writeFile('data/asset.digest.json', JSON.stringify(assets, null, 4), 'utf8');
+
+        return assets;
+    }
+}
+
+async function downloadAssets(client: pogobuf.Client, assets: POGOProtos.Networking.Responses.GetAssetDigestResponse) {
+    if (fs.existsSync('data/.skip')) return;
+
+    let idx = 0;
+    logger.info('Starting to download assets...');
+    await Bluebird.map(assets.digest, async asset => {
+        logger.info('Get asset from %s (%d, %d)', asset.bundle_name, ++idx, assets.digest.length);
+        let response = await client.getDownloadURLs([ asset.asset_id ]);
+        let data = await request.get(response.download_urls[0].url);
+        await fs.writeFile(`data/${asset.bundle_name}`, data);
+        logger.info('%s done. (%d, %d)', asset.bundle_name, ++idx, assets.digest.length);
+        await Bluebird.delay(_.random(450, 550));
+    });
+
+    await fs.writeFile('data/.skip', 'skip', 'utf8');
+}
 
 async function Main() {
     try {
+        await fs.mkdir('data');
+
+        logger.info('Login...');
         let login = new pogobuf.PTCLogin();
-        login.setProxy(process.env.proxy);
+        if (process.env.proxy) login.setProxy(process.env.proxy);
         let token = await login.login(process.env.user, process.env.password);
 
         let client = new pogobuf.Client({
@@ -27,12 +70,14 @@ async function Main() {
 
         await client.init(false);
 
-        await client.batchStart().batchCall();
+        logger.info('First request...');
+        client.batchStart().getPlayer('FR', 'en', 'Europe/Paris');
+        await client.batchCall();
 
-        let assets = await client.getAssetDigest(POGOProtos.Enums.Platform.IOS, '', '', '', 5702);
-        let assetsDigests = assets.digest;
+        let assets = await getAssetDigest(client);
+        logger.info('%d assets to download', assets.digest.length);
 
-        await fs.writeFile('assets.digest.json', JSON.stringify(assetsDigests, null, 4), 'utf8');
+        await downloadAssets(client, assets);
 
     } catch (e) {
         logger.error(e);
